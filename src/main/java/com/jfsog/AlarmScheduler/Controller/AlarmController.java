37cf6962d56365780cd0655a111e4647c7da6ca5
jfsog <Felipe.sg@protonmail.com>
2024-10-25 21:23:24 -0300

    Mapeamento e persistência.

--- src/main/java/com/jfsog/AlarmScheduler/Controller/AlarmController.java
+++ src/main/java/com/jfsog/AlarmScheduler/Controller/AlarmController.java
@@ -1,27 +1,52 @@
 package com.jfsog.AlarmScheduler.Controller;
 
 import com.jfsog.AlarmScheduler.Services.Alarm;
+import com.jfsog.AlarmScheduler.repository.AlarmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.time.OffsetDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
 @Controller
 public class AlarmController {
+    private final AlarmRepository alarmRepository;
+    @Autowired
+    public AlarmController(AlarmRepository alarmRepository) {
+        this.alarmRepository = alarmRepository;
+    }
     @QueryMapping
-    public Alarm getAlarm(@Argument String id) {
-        var time = OffsetDateTime.now();
-        var val = time.plusMinutes(5);
-        return Alarm.builder().id(UUID.randomUUID()).action("Teste action").time(val).build();
+    public Alarm getAlarm(@Argument UUID id) {
+        return alarmRepository.findById(id).orElseThrow(() -> new RuntimeException("Alarm not found"));
+    }
+    @MutationMapping
+    public String deleteAlarm(@Argument UUID id) {
+        var alarm=alarmRepository.findById(id).orElseThrow(() -> new RuntimeException("Alarm not found"));
+        alarmRepository.delete(alarm);
+        return alarm.getAction();
     }
     @QueryMapping
     public List<Alarm> GetAllAlamrs() {
-        var time = OffsetDateTime.now();
-        var val = time.plusMinutes(5);
-        var alarm = Alarm.builder().id(UUID.randomUUID()).action("Teste action all").time(val).build();
-        return List.of(alarm);
+        return alarmRepository.findAll().stream().sorted(Comparator.comparing(Alarm::getDateTime)).toList();
+    }
+    @MutationMapping
+    public Alarm CreateAlarm(@Argument OffsetDateTime dateTime, @Argument String action) {
+        return alarmRepository.saveAndFlush(Alarm.builder()
+                                                 .id(UUID.randomUUID())
+                                                 .action(action)
+                                                 .dateTime(dateTime)
+                                                 .build());
+    }
+    @MutationMapping
+    public Alarm updateAlarm(@Argument UUID id, @Argument OffsetDateTime dateTime, @Argument String action) {
+        var a = alarmRepository.findById(id).orElseThrow(() -> new RuntimeException("Alarm not found"));
+        if (dateTime != null) a.setDateTime(dateTime);
+        if (action != null) a.setAction(action);
+        return alarmRepository.save(a);
     }
 }
---
+++ src/main/java/com/jfsog/AlarmScheduler/repository/AlarmRepository.java
@@ -0,0 +1,11 @@
+package com.jfsog.AlarmScheduler.repository;
+
+import com.jfsog.AlarmScheduler.Services.Alarm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AlarmRepository extends JpaRepository<Alarm, UUID> {
+}
